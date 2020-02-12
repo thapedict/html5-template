c749fe848e042582705c59059dec2d4aa32b9fce
Thapelo Moeti <benzitto@gmail.com>
2020-02-12 19:42:15 +0200

    fixed some gulp + browsersync issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,50 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-gulp.task('sass',()=>{
-    return gulp.src('src/scss/*.scss')
+const paths = {
+    app: './app/',
+    devCSS: './src/scss/',
+    css: './app/css/',
+    devJS: './src/js/',
+    js: './app/js/'
+};
+
+const globs = {
+    scss: paths.devCSS + '*.scss',
+    css: paths.css + '*.css',
+    html: paths.app + '*.html',
+    js: paths.js + '*.js'
+};
+
+gulp.task('sass',function(){
+    return gulp.src(paths.devCSS + '*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
 });
+
+function reload(done){
+    browserSync.reload();
+    //done();
+}
+
+function serve( done ){
+    browserSync.init({
+        server: {
+            baseDir: paths.app
+        }
+    });
+    done();
+};
+
+function watcher() {
+    gulp.watch(globs.html).on('change', reload);
+    gulp.watch(globs.scss,gulp.series('sass'));
+}
+
+gulp.task('watch',
+        gulp.series( serve, watcher )
+);
